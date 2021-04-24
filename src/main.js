2da656123ae1183e95a4330e090624838a89d727
Jakub Kapala <kubusxzx@gmail.com>
2021-04-24 21:25:32 +0200

    dependencies update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import vuex from './store/store.js'
+import { BootstrapVue } from 'bootstrap-vue'
+import './App.scss'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
+  store: vuex,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/store.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex);
+
+const state = {
+
+}
+
+// getters
+const getters = {
+
+}
+
+// actions
+const actions = {
+
+}
+const mutations = {
+
+}
+
+//export store 
+export default new Vuex.Store({
+    state,
+    getters,
+    actions,
+    mutations
+})
